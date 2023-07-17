5283ad0ea37a265753e02dde2d1514ddbd6b6cbe
Rana12356 <rana15-12356@diu.edu.bd>
2023-07-17 21:25:56 +0600

    payment gateway sslz commerce connected with the project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SslCommerzPaymentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyCommerceController;
 use App\Http\Controllers\CartController;
@@ -40,6 +41,20 @@ Route::get('/customer-logout', [CustomerAuthController::class, 'logout'])->name(
 Route::get('/customer-dashboard', [CustomerAuthController::class, 'dashboard'])->name('customer.dashboard');
 Route::get('/customer-profile', [CustomerAuthController::class, 'profile'])->name('customer.profile');
 Route::get('/customer-order', [CustomerOrderController::class, 'allOrder'])->name('customer.order');
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
 
 
 
